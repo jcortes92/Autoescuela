5ebc557eb1262beeae2526e3cc1899f745e35832
jcortes92 <jcortesanguita@gmail.com>
2014-05-14 23:50:31 +0200

    Limpieza de código

--- script.js
+++ script.js
@@ -1,25 +1,19 @@
 /**
- * @author Jorge Cortés
- */
- // (function() {
- 	var titles = [];
- 	var test = [];
-			var allData = []; //contenedor de contenedores de preguntas. Cada elemento será un array conteniendo una pregunta completa (con todos sus elementos)
-			var bolsaNumeros = [];
-			var aleatorios=[];
-			var contadorPreguntas = 0;
-			var respuestas=[];
+* @author Jorge Cortés
+*/
+// (function() {
+	var titles = [];
+	var test = [];
+var allData = []; //contenedor de contenedores de preguntas. Cada elemento será un array conteniendo una pregunta completa (con todos sus elementos)
+var bolsaNumeros = [];
+var aleatorios=[];
+var contadorPreguntas = 0;
+var respuestas=[];
 
-			function mostrarSiguientes (){
-				var y = contadorPreguntas;
-				$("#preguntasMostradas").empty();
-				// for (contadorPreguntas; contadorPreguntas < 3; contadorPreguntas++) {
-					for(var x = contadorPreguntas; x < y+3; x++){
-		// for (var y = 0; y < test[0].length; y++) {
-		// 	//Comprobación de todas las componentes del array
-		// 	//console.log("allData[" + x + "][" + y + "]=" + allData[x][y]);
-		// }	
-		
+function mostrarSiguientes (){
+	var y = contadorPreguntas;
+	$("#preguntasMostradas").empty();
+	for(var x = contadorPreguntas; x < y+3; x++){
 		//Si la imagen es nula, no mostrarla.
 		var img = test[contadorPreguntas][6];
 		if(img!="no_image.png") img = '<img src="src/imagenes/'+ test[contadorPreguntas][6]+'"" style="float:right;" >';
@@ -27,7 +21,7 @@
 
 		//Pasar de mostrar todas a mostrarlas en paquetes de tres, reemplazando las que haya en pantalla.
 		$("#preguntasMostradas").append('<div class="preguntas"><br/>'+img+
-			'<p class="pregunta">' + (contadorPreguntas+1).toString()+'. '+test[contadorPreguntas][0] + '</p>' + 
+			'<p class="pregunta"><span class="numPregunta">' + (contadorPreguntas+1).toString()+'.</span>'+test[contadorPreguntas][0] + '</p>' + 
 			'<div class="respuesta"'+
 			'<li><ol><input type="radio" name="'+contadorPreguntas+'" value="1" >a) ' + test[contadorPreguntas][1] + '</ol>'+
 		'<ol><input type="radio"name="'+contadorPreguntas+'" value="2">b) ' + test[contadorPreguntas][2]+'</ol>'+
@@ -38,7 +32,6 @@
 	}
 }
 
-
 $(document).ready(function() {
 
 
@@ -46,57 +39,57 @@ $(document).ready(function() {
 // 	mostrarSiguientes();
 // });
 $('#botonSiguiente').click(mostrarSiguientes);
-	
 
 
-	function parse(document) {
-		$(document).find("PREGUNTA").each(function() {
-					//contenedor de pregunta completa (item), con todos sus elementos.
-					var item = [];
 
-					//bucle que busca todas las componentes de cada item y las mete en un array
-					$(this).find("*").each(function() {
-						item.push($(this).text());
-					});
-					
-					//mete ese array en la siguiente posisición del array allData
-					allData.push(item);
-					// console.log(item);
-					// console.log(allData);
-				});
+function parse(document) {
+	$(document).find("PREGUNTA").each(function() {
+//contenedor de pregunta completa (item), con todos sus elementos.
+var item = [];
 
-				//SELECCIÓN ALEATORIA DE PREGUNTAS DE LA BASE DE DATOS
-				for(var i=0;i<allData.length;i++){
-					bolsaNumeros[i] = i; //aqui tenemos los 140 numeros posibles
-				}
-				for(var i=0;i<30;i++){
-					aleatorios[i] = Math.floor((Math.random() * bolsaNumeros.length)+1);
-					bolsaNumeros.splice(aleatorios[i],1);
-				}
-				for (var i =0;i<30;i++){
-					test[i] = allData[aleatorios[i]]
-				}
+//bucle que busca todas las componentes de cada item y las mete en un array
+$(this).find("*").each(function() {
+	item.push($(this).text());
+});
 
-				//console.log(allData);
-						// for (contadorPreguntas; contadorPreguntas < 3; contadorPreguntas++) {
-						// 	// for (var y = 0; y < test[0].length; y++) {
-						// 	// 	//Comprobación de todas las componentes del array
-						// 	// 	//console.log("allData[" + x + "][" + y + "]=" + allData[x][y]);
-						// 	// }
+//mete ese array en la siguiente posisición del array allData
+allData.push(item);
+// console.log(item);
+// console.log(allData);
+});
 
-						// 	//Si la imagen es nula, no mostrarla.
-						// 	var img = test[contadorPreguntas][6];
-						// 	if(img!="no_image.png") img = '<img src="src/imagenes/'+ test[contadorPreguntas][6]+'"" style="float:right;" >';
-						// 	else img = "";
+//SELECCIÓN ALEATORIA DE PREGUNTAS DE LA BASE DE DATOS
+for(var i=0;i<allData.length;i++){
+bolsaNumeros[i] = i; //aqui tenemos los 140 numeros posibles
+}
+for(var i=0;i<30;i++){
+	aleatorios[i] = Math.floor((Math.random() * bolsaNumeros.length)+1);
+	bolsaNumeros.splice(aleatorios[i],1);
+}
+for (var i =0;i<30;i++){
+	test[i] = allData[aleatorios[i]]
+}
 
-						// 	//Pasar de mostrar todas a mostrarlas en paquetes de tres, reemplazando las que haya en pantalla.
-						// 	$("#preguntasMostradas").append('<div class="preguntas"><br/>'+img+
-						// 		'<p class="pregunta">' + (contadorPreguntas+1).toString()+'. '+test[contadorPreguntas][0] + '</p>' + 
-						// 		'<div class="respuesta"'+
-						// 		'<li><ol><input type="radio" name="'+contadorPreguntas+'" value="1" >a) ' + test[contadorPreguntas][1] + '</ol>'+
-						// 	'<ol><input type="radio"name="'+contadorPreguntas+'" value="2">b) ' + test[contadorPreguntas][2]+'</ol>'+
-						// 	'<ol><input type="radio"name="'+contadorPreguntas+'" value="3">c) ' + test[contadorPreguntas][3]+'</ol>'+
-						// 	'</li></div></div>');
+//console.log(allData);
+// for (contadorPreguntas; contadorPreguntas < 3; contadorPreguntas++) {
+// 	// for (var y = 0; y < test[0].length; y++) {
+// 	// 	//Comprobación de todas las componentes del array
+// 	// 	//console.log("allData[" + x + "][" + y + "]=" + allData[x][y]);
+// 	// }
+
+// 	//Si la imagen es nula, no mostrarla.
+// 	var img = test[contadorPreguntas][6];
+// 	if(img!="no_image.png") img = '<img src="src/imagenes/'+ test[contadorPreguntas][6]+'"" style="float:right;" >';
+// 	else img = "";
+
+// 	//Pasar de mostrar todas a mostrarlas en paquetes de tres, reemplazando las que haya en pantalla.
+// 	$("#preguntasMostradas").append('<div class="preguntas"><br/>'+img+
+// 		'<p class="pregunta">' + (contadorPreguntas+1).toString()+'. '+test[contadorPreguntas][0] + '</p>' + 
+// 		'<div class="respuesta"'+
+// 		'<li><ol><input type="radio" name="'+contadorPreguntas+'" value="1" >a) ' + test[contadorPreguntas][1] + '</ol>'+
+// 	'<ol><input type="radio"name="'+contadorPreguntas+'" value="2">b) ' + test[contadorPreguntas][2]+'</ol>'+
+// 	'<ol><input type="radio"name="'+contadorPreguntas+'" value="3">c) ' + test[contadorPreguntas][3]+'</ol>'+
+// 	'</li></div></div>');
 
 mostrarSiguientes();
 
@@ -106,13 +99,13 @@ mostrarSiguientes();
 
 
 $.ajax({
-			url : 'src/preguntas.xml', // name of file you want to parse
-			dataType : "xml",
-			success : parse,
-			error : function() {
-				alert("Error: Something went wrong");
-			}
-		});
+url : 'src/preguntas.xml', // name of file you want to parse
+dataType : "xml",
+success : parse,
+error : function() {
+	alert("Error: Something went wrong");
+}
+});
 
 
 
